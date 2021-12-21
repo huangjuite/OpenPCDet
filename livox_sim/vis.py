53c0c25e50b0846af9e58b84c6d37d3fa5d28207
huangjuite <huangjuite@gmail.com>
2021-12-21 13:08:35 -0800

    vis

---
+++ livox_sim/vis.py
@@ -0,0 +1,114 @@
+import os
+import open3d as o3d
+import numpy as np
+import pandas as pd
+import torch
+
+label_map = {'car': 'Car', 'truck': 'Car', 'bus': 'Car',
+             'bimo': 'Cyclist', 'pedestrian': 'Pedestrian'}
+
+
+def check_numpy_to_torch(x):
+    if isinstance(x, np.ndarray):
+        return torch.from_numpy(x).float(), True
+    return x, False
+
+
+def rotate_points_along_z(points, angle):
+    """
+    Args:
+        points: (B, N, 3 + C)
+        angle: (B), angle along z-axis, angle increases x ==> y
+    Returns:
+
+    """
+    points, is_numpy = check_numpy_to_torch(points)
+    angle, _ = check_numpy_to_torch(angle)
+
+    cosa = torch.cos(angle)
+    sina = torch.sin(angle)
+    zeros = angle.new_zeros(points.shape[0])
+    ones = angle.new_ones(points.shape[0])
+    rot_matrix = torch.stack((
+        cosa,  sina, zeros,
+        -sina, cosa, zeros,
+        zeros, zeros, ones
+    ), dim=1).view(-1, 3, 3).float()
+    points_rot = torch.matmul(points[:, :, 0:3], rot_matrix)
+    points_rot = torch.cat((points_rot, points[:, :, 3:]), dim=-1)
+    return points_rot.numpy() if is_numpy else points_rot
+
+
+def boxes_to_corners_3d(boxes3d):
+    """
+        7 -------- 4
+       /|         /|
+      6 -------- 5 .
+      | |        | |
+      . 3 -------- 0
+      |/         |/
+      2 -------- 1
+    Args:
+        boxes3d:  (N, 7) [x, y, z, dx, dy, dz, heading], (x, y, z) is the box center
+
+    Returns:
+    """
+    boxes3d, is_numpy = check_numpy_to_torch(boxes3d)
+
+    template = boxes3d.new_tensor((
+        [1, 1, -1], [1, -1, -1], [-1, -1, -1], [-1, 1, -1],
+        [1, 1, 1], [1, -1, 1], [-1, -1, 1], [-1, 1, 1],
+    )) / 2
+
+    corners3d = boxes3d[:, None, 3:6].repeat(1, 8, 1) * template[None, :, :]
+    corners3d = rotate_points_along_z(
+        corners3d.view(-1, 8, 3), boxes3d[:, 6]).view(-1, 8, 3)
+    corners3d += boxes3d[:, None, 0:3]
+
+    return corners3d.numpy() if is_numpy else corners3d
+
+
+def read_txt(f):
+    pnp = pd.read_csv(f, sep=",", header=None).to_numpy().astype(
+        np.float32)[:, :4]
+    pnp[:, 3] = 0
+    return pnp
+
+
+def read_label(f):
+    la = pd.read_csv(f, sep=",", header=None)
+    det_lines = []
+    dets = []
+    for l in la.iterrows():
+        if l[1][1] not in label_map.keys():
+            continue
+        ctype = label_map[l[1][1]]
+        pos = l[1][2:5].to_numpy().astype(np.float32)
+        length = float(l[1][5])
+        width = float(l[1][6])
+        height = float(l[1][7])
+        ry = float(l[1][8])
+        det_lines.append('%s %d %d %d %d %d %d %d %f %f %f %f %f %f %f\n' % (
+            ctype, 0, 0, 0, 1, 2, 3, 4, height, width, length, pos[0], pos[1], pos[2], ry))
+        dets.append([pos[0], pos[1], pos[2], length, width, height, ry])
+
+    return dets
+
+
+if __name__ == '__main__':
+    points = read_txt('point.txt')
+    labels = np.array(read_label('anno.txt'))
+    boxes = boxes_to_corners_3d(labels)
+
+    geometry = []
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points[:, :3])
+    geometry.append(pcd)
+
+    for i, bo in enumerate(boxes):
+        boxP = o3d.utility.Vector3dVector(bo)
+        o3dBox = o3d.geometry.OrientedBoundingBox().create_from_points(boxP)
+        # o3dBox.color = colors[labels[i]-1]
+        geometry.append(o3dBox)
+
+    o3d.visualization.draw_geometries(geometry)
